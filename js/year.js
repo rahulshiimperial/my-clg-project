ffd33312875130a7affd814717855fa76ad7e344
Rahul Rai <rairahulmrt@gmail.com>
2024-11-20 21:34:50 +0530

    my college prject

---
+++ js/year.js
@@ -0,0 +1,7 @@
+// currentYearScript.js
+
+// Get the current year using JavaScript
+const currentYear = new Date().getFullYear();
+
+// Display the current year in the designated HTML element
+document.getElementById("currentYear").textContent = currentYear;
